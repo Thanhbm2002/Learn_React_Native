e7af52d2dc70f97d6ebb374de92c9c11a1dc7159
Thanhbm2002 <thanhbm2002@example.com>
2025-04-25 20:26:59 +0700

    First commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>TextComp</Text>
+      </View>
+      <Text style={styles.hello1}>Hello World
+      </Text>
+      <Text>Hello World 2</Text>
     </View>
   );
 }
@@ -17,4 +21,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: "red",
+    fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10
+  },
+  header: {
+    fontSize:30,
+    fontWeight: "600"
+  }
 });
