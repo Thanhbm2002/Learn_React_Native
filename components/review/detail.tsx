1664ecb8a70006559678551b5a10113c3519af3a
Thanhbm2002 <thanhbm2002@example.com>
2025-05-09 07:07:07 +0700

    passing parameter to Routes

--- components/review/detail.tsx
+++ components/review/detail.tsx
@@ -12,9 +12,9 @@ const DetailScreen = (props: any) => {
     return(
         <View>
             <Text style={styles.review}>Review Detail</Text>
-            <Text>{route.params?.id}</Text>
-            <Text>{route.params?.title}</Text>
-            <Text>{route.params?.star}</Text>
+            <Text style={styles.reviewText}>{route.params?.id}</Text>
+            <Text style={styles.reviewText}>{route.params?.title}</Text>
+            <Text style={styles.reviewText}>{route.params?.star}</Text>
             <Button title="Go Home"
                          onPress={()=> navigation.navigate("Home")}
                         />
@@ -26,6 +26,11 @@ const styles = StyleSheet.create({
     review: {
         fontSize: 30,
         fontFamily: OPENSANS_REGULAR,
+    },
+    reviewText: {
+        fontSize: 25,
+        fontFamily: OPENSANS_REGULAR,
+        padding: 15,
     }
 })
 export default DetailScreen;
